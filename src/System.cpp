061e34064024df2a79b2089967cb364d8359354a
Struan Robertson <struanhrobertson@gmail.com>
2009-07-12 19:11:11 +0000

    More linesearch material.
    SVN: r414@trunk

--- src/System.cpp
+++ src/System.cpp
@@ -658,28 +658,87 @@ namespace mesmer
 
   void System::LineSearch(const int varID) {
 
-	  // First catch your hare ... need to bracket the minimum. To do this
-	  // use the parameter limits supplied. 
+    static const double Gold = (3.0 - sqrt(5.0))/2.0 ;
+    static const double GRatio = (1.0 - Gold)/Gold ;
+    static const double tol = 1.0e-8 ;
+    static const int limit = 100 ;
 
-      double a, b, stepsize ;
-	  Rdouble::withRange()[varID]->get_range(a, b, stepsize);
+    // First catch your hare ... need to bracket the minimum. To do this
+    // use the parameter limits supplied. 
 
-	  // Calculate chi2 at the upper and lower points.
-	  *Rdouble::withRange()[varID]=a ;
-      double chi2a ;
-      calculate(chi2a);
+    double a, b, stepsize ;
+    Rdouble::withRange()[varID]->get_range(a, b, stepsize);
 
-	  *Rdouble::withRange()[varID]=b ;
-      double chi2b ;
-      calculate(chi2b);
+    // Calculate chi2 at the upper and lower points.
+    *Rdouble::withRange()[varID]=a ;
+    double chi2a ;
+    calculate(chi2a);
 
-      // Determine the direaction of search.
+    *Rdouble::withRange()[varID]=b ;
+    double chi2b ;
+    calculate(chi2b);
 
-	  double direction = (chi2a > chi2b)? 1.0 : -1.0 ;
+    // Alter the direction of search so that we are always going down hill.
 
-	  // Follow gradient down hill to estimate loation of the next point.
+    if (chi2a < chi2b) {
+      double tmp = a ;
+      a = b ;
+      b = tmp ;
+      tmp = chi2a ;
+      chi2a = chi2b ;
+      chi2b = tmp ;
+    }
 
+    // Follow gradient down hill to estimate loation of the next point.
+
+    double c = b + GRatio*(b - a) ;
+
+    // Calculate a new value of chi2 for the new parameter value. 
+
+    *Rdouble::withRange()[varID]=c ;
+    double chi2c ;
+    calculate(chi2c);
+
+    // Repeat the search until a minimum has been bracketed or
+    // the search limit has been reached. 
+
+    int count(0) ;
+    while (count < limit && chi2c < chi2b) {
+      count++ ;
+
+      // Shift values so as to maintain bracketing.
+      a     = b ;
+      chi2a = chi2b ;
+      b     = c ;
+      chi2b = chi2c ;
+
+      // Determine next estimate of lower bracket point.
+
+      c = b + GRatio*(b - a) ;
+      *Rdouble::withRange()[varID]=c ;
+      calculate(chi2c);
+    }
+
+    // At this point the minimum should be bracketed, so 
+    // use golden section search to refine the minimum.
+
+    double x = b + Gold*(c - b) ;
+    *Rdouble::withRange()[varID]=x ;
+    double chi2x ;
+    calculate(chi2x);
+
+    while(true){
+      if (chi2x < chi2b) {
+        a = b ;
+        chi2a = chi2b ;
+        b = x ;
+        chi2b = chi2x ;
+
+      } else {
+        c = x ;
+        chi2c = chi2x ;
+      }
+    }
   }
-
 
 }//namespace
