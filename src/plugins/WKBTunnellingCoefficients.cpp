68baa55783b051f4f75464931119fc0f3fe0b068
Struan Robertson <struanhrobertson@gmail.com>
2011-04-17 17:39:03 +0000

    Refactoring of WKB tunnelling method.
    SVN: r647@trunk

--- src/plugins/WKBTunnellingCoefficients.cpp
+++ src/plugins/WKBTunnellingCoefficients.cpp
@@ -13,8 +13,8 @@
 #include <string>
 #include <stdexcept>
 #include "../System.h"
-#include "../AssociationReaction.h"
-#include "../IrreversibleExchangeReaction.h"
+#include "../Reaction.h"
+
 using namespace Constants;
 using namespace std;
 
@@ -31,12 +31,16 @@ namespace mesmer
 
     virtual bool calculateCellTunnelingCoeffs(Reaction* pReact, std::vector<double>& TunnelingProbability);
 
+  private:
+
+    // Read potential barrier details
+    bool ReadPotential(Reaction* pReact, vector<double> &potential, vector<double> &distance, double &mu) ;
+
     //Integration quadrature to obtain theta
-    double WKBTunnellingCoefficients::Theta(vector<double> pot , vector<double> MEP , int Ene , double mu);
+    double Theta(const vector<double> &pot , const vector<double> &MEP , int Ene , double mu);
 
     //Integation quadrature for Boltzmann averaging to obtain macroscopic transmission coefficients 
-    double WKBTunnellingCoefficients::Trans(vector<double> TunProb , double Vmax, double beta );
-
+    double Trans(const vector<double> &TunProb , double Vmax, double beta );
 
   };
 
@@ -47,40 +51,75 @@ namespace mesmer
 
   bool WKBTunnellingCoefficients::calculateCellTunnelingCoeffs(Reaction* pReact, vector<double>& TunnelingProbability){
 
-
-
-
-
     //TZ is the zpe of the TS
     const double TZ = pReact->get_relative_TSZPE();
     //barrier0 & barrier1 are the zpe corrected barrier heights in the forward/reverse directions
     const int barrier0 = int(TZ) - int(pReact->get_relative_rctZPE());
     const int barrier1 = int(TZ) - int(pReact->get_relative_pdtZPE());
 
+    // Read in potential barrier details.
+    vector<double> potential, distance ;
+    double mu(0.0) ;
+    if (!ReadPotential(pReact, potential, distance, mu))
+      return false ;
 
-    //get properties of vectors in which to include transmission coefficients
+    // Set transmission coefficients to 0 where no tunneling is possible;
+    // where tunneling may occur, the transmission coefficients are calculated using a wkb formalism
+    // as described by  B. C. Garrett and D. G. Truhlar, J. Phys. Chem., 1979, 83, 292
     const int MaximumCell = pReact->get_reactant()->getEnv().MaxCell;
     TunnelingProbability.clear();
     TunnelingProbability.resize(MaximumCell);
+    for(int i = 0; i < MaximumCell; ++i){
+      int E = i - barrier0;
+      TunnelingProbability[i] = 0.0;
+      if ((E + barrier1) < 0) {
+        TunnelingProbability[i] = 0.0;
+      } else if (E <= 0) {
+        TunnelingProbability[i] = 1.0 /(1.0 + ( exp (2.0 * Theta( potential, distance , E + barrier0 , mu)))) ;
+      } else if (E > 0 && E <= barrier0 ) { 
+        //non classical reflection above the barrier
+        TunnelingProbability[i] = 1.0 - (1.0 /(1.0 + ( exp (2.0 * Theta( potential, distance , barrier0 - E, mu))))) ;
+      } else if (E > barrier0) {
+        TunnelingProbability[i] = 1.0 ;
+      } else {
+        // This branch should never be executed.
+      }
+    }
 
-    Molecule * p_TransitionState = pReact->get_TransitionState();
+    // Calculatate macroscopic transmission coefficient for testing purposes
+    const double beta = pReact->getEnv().beta ; 
+    double TransmissionCoefficient = ( 1.0 + ( 2.0 * beta )*Trans( TunnelingProbability , barrier0, beta ) );
 
-    // read input data for barrier IRC
+    if (pReact->getFlags().TunnellingCoeffEnabled){
+      ctest << "\nTransmission Coefficient for : " << pReact->getName() << "=" << TransmissionCoefficient;
+      ctest << "\nTunneling coefficients for: " << pReact->getName();
 
-    PersistPtr pptran = p_TransitionState->get_PersistentPointer();
+      for(size_t i(0) ; i < TunnelingProbability.size() ; ++i){
+        ctest << TunnelingProbability[i] << endl;
+      }
+      ctest << "}\n";
+    }
 
+    return true;
+  }
+
+  // Read potential barrier details
+  bool WKBTunnellingCoefficients::ReadPotential(Reaction* pReact, vector<double> &potential, vector<double> &distance, double &mu) {
+
+    // Read input data for barrier IRC.
+
+    PersistPtr pptran = pReact->get_TransitionState()->get_PersistentPointer();
+    
     PersistPtr pp = pptran->XmlMoveTo("me:IRCPotential") ;
-    if(!pp)
-    {  
+    if (!pp) {  
       // Force program to close if not potential information available and print error message
       throw (std::runtime_error("Error: WKB calculation cannot proceed without a PES")); 
     }
     const char* p = pp->XmlReadValue("units", optional);
     string units = p ? p : "kJ/mol";
-    double mu = pp->XmlReadDouble("ReducedMass", optional);
-    if(IsNan(mu)) mu = 1.;
-    vector<double> potential ;
-    vector<double> distance ;
+    
+    mu = pp->XmlReadDouble("ReducedMass", optional);
+    if(IsNan(mu)) mu = 1.0 ;
 
     while(pp = pp->XmlMoveTo("me:PotentialPoint"))
     {
@@ -96,81 +135,40 @@ namespace mesmer
       potential.push_back(convertedpotentialPoint) ;
     }
 
-    //set transmission coefficients to 0 where no tunneling is possible;
-    //where tunneling may occur, the transmission coefficients are calculated using a wkb formalism
-    // as described by  B. C. Garrett and D. G. Truhlar, J. Phys. Chem., 1979, 83, 292
-
-    for(int i = 0; i < MaximumCell; ++i){
-      int E = i - barrier0;
-      if ((E + barrier1) < 0){
-        TunnelingProbability[i] = 0.0;
-      }
-      else if (E <= 0){
-        TunnelingProbability[i] = 1 / (1 + ( exp ( 2 * Theta( potential, distance , E + barrier0 , mu)))) ;
-
-      }
-
-      //non classical reflection above the barrier
-      else if (  E > 0 && E <= barrier0 ){
-        TunnelingProbability[i] =1 -( 1 / (1 + ( exp ( 2 * Theta( potential, distance , 2*barrier0 - (E + barrier0), mu))))) ;
-
-      }
-      else if ( E > barrier0){
-        TunnelingProbability[i] = 1;
-      }
-
-      {
-
-        if(IsNan(TunnelingProbability[i])) TunnelingProbability[i] = 0.0;
-      }
-    }
-    const double beta = pReact->getEnv().beta * 1./(SpeedOfLight_in_cm * PlancksConstant_in_JouleSecond) ; //Beta in J^-1
-    // Calculatate macroscopic transmission coefficient for testing purposes
-    double TransmissionCoefficient = ( 1 + ( 2 * beta )*Trans( TunnelingProbability , barrier0, beta ) );
-
-    if (pReact->getFlags().TunnellingCoeffEnabled){
-      ctest << "\nTransmission Coefficient for : " << pReact->getName() << "=" << TransmissionCoefficient;
-      ctest << "\nTunneling coefficients for: " << pReact->getName();
-
-      for(int i = 0; i < MaximumCell; ++i){
-        ctest << TunnelingProbability[i] << endl;
-      }
-      ctest << "}\n";
-    }
-
-    return true;
+    return true ;
   }
 
-  double WKBTunnellingCoefficients::Theta(vector<double> pot , vector<double> MEP , int Ene , double mu){
-    double integral=0.0;
-    //Quadrature for integration, just using a simple trapezium like rule for now, need to include quadrature described in the reference above.
-    for(int i = 0; i < pot.size()-1 ; ++i){
+  double WKBTunnellingCoefficients::Theta(const vector<double> &pot, const vector<double> &MEP, int Ene, double mu){
+
+    // Quadrature for integration, just using a simple trapezium like rule for now,
+    // need to include quadrature described in the reference above.
+    double integral(0.0);
+    double energy = double(Ene)*SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond ;
+    for(size_t i(0) ; i < pot.size()-1 ; ++i){
       //Only integrate over energies > current cell energy
-      if (pot[i]>= (Ene*SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond)){; // convert energy into joules
-      double A = MEP[i];
-      double fA = sqrt ( 2  * mu * amu * fabs( pot[i]- (Ene*SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond)));
-      double B = MEP[i+1];
-      double fB = sqrt ( 2  * mu * amu * fabs(  pot[i+1] - (Ene*SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond)));
+      if (pot[i] >= energy) { 
+        double fA = sqrt ( 2.0  * mu * amu * fabs(pot[i]   - energy));
+        double fB = sqrt ( 2.0  * mu * amu * fabs(pot[i+1] - energy));
 
-      integral += (B - A) * ((fA + fB) / 2);
+        integral += (MEP[i+1] - MEP[i]) * ((fA + fB) / 2);
       }
-
     }
 
-    double Theta = (integral * 2 * M_PI) / PlancksConstant_in_JouleSecond;
-    return Theta;
+    return (integral * 2.0 * M_PI) / PlancksConstant_in_JouleSecond;
   }
-  
-  //Quadrature for Boltzmann averaging
-  double WKBTunnellingCoefficients::Trans(vector<double> TunProb , double Vmax, double beta ){
-    double Trans=0.0;
-    for(int i = 0; i < Vmax; ++i){
-      double fA = sinh( (Vmax - i)*SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond* beta ) * TunProb[i];
-      double fB = sinh( (Vmax - (i+1))*SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond * beta) * TunProb[i + 1];
 
-      Trans +=  SpeedOfLight_in_cm*PlancksConstant_in_JouleSecond*(fA + fB) / 2 ;
+  double WKBTunnellingCoefficients::Trans(const vector<double> &TunProb , double Vmax, double beta ){
+
+    // Trapezium quadrature for Boltzmann averaging. Units of energy are cm-1.
+    double Trans(0.0); 
+    double dEne(1.0) ; // cm -1. 
+    for(int i = 0; i < int(Vmax) ; ++i){
+      double fA = sinh( (Vmax - double(i))   * beta) * TunProb[i];
+      double fB = sinh( (Vmax - double(i+1)) * beta) * TunProb[i + 1];
+
+      Trans += (fA + fB) * 0.5 ;
     }
-    return Trans;
+    return Trans*dEne ;
   }
 
 }//namespace
