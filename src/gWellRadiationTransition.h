1c80982712df21aa25f8234ef8fb186d4b62a5f4
Struan Robertson <struanhrobertson@gmail.com>
2023-02-18 17:28:21 +0000

    Tided Planck distribution.
    SVN: r1646@trunk

--- src/gWellRadiationTransition.h
+++ src/gWellRadiationTransition.h
@@ -55,9 +55,6 @@ namespace mesmer
       // return 8.0 * M_PI * pow(ene, 3.0) * 1.0e+06 * PlancksConstant_in_JouleSecond / (exp(beta * ene) - 1.0);
       double radiance(0.0);
       const double h = PlancksConstant_in_JouleSecond;
-      const double c = SpeedOfLight_in_cm;
-
-      double nu = ene * c;
 
       radiance = 8.0 * M_PI * h * pow(ene, 3.0) * 1.0e+06 / (exp(beta * ene) - 1.0);
 
