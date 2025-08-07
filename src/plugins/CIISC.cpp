5ce5472711f8d2f7477bf11c8136f3f932a81099
Struan H Robertson <struanhrobertson@gmail.com>
2025-08-07 20:40:54 +0100

    Initial implmentations of plugin classes for the support of Internal Conversion and Inter-System Crossing.

--- src/plugins/CIISC.cpp
+++ src/plugins/CIISC.cpp
@@ -23,7 +23,7 @@ namespace mesmer
   {
   public:
     ///Constructor which registers with the list of MicroRateCalculators in the base class
-    CIISC(const char* id) : m_id(id) { Register(); }
+    CIISC(const char* id) : m_CIISCProb(1.0), m_id(id) { Register(); }
 
     virtual ~CIISC() {}
     virtual const char* getID() { return m_id; }
@@ -37,6 +37,9 @@ namespace mesmer
     virtual double get_ThresholdEnergy(Reaction* pReac) { return max(0.0,(pReac->get_relative_pdtZPE() - pReac->get_relative_rctZPE())) ; };
 
   private:
+    
+    double m_CIISCProb; // Collisional transition probability.
+    
     const char* m_id;
   };
 
@@ -45,8 +48,12 @@ namespace mesmer
   CIISC theCIISC("CIISC");
   //************************************************************
 
-  bool CIISC::ParseData(PersistPtr)
+  bool CIISC::ParseData(PersistPtr pp)
   {
+    double CIISCProb = pp->XmlReadDouble("me:CIISCProbability");
+
+    m_CIISCProb = (IsNan(CIISCProb)) ? 1.0 : min(max(CIISCProb,0.0),1.0);
+
     return true;
   }
 
@@ -63,7 +70,7 @@ namespace mesmer
     double coll_frq = pReact->get_reactant()->getColl().get_collisionFrequency();
     for (size_t i(0); i < MaximumCell; ++i) {
       // Calculate microcanonical rate coefficients using CIISC expression.
-      rxnFlux[i] = 0.01 * coll_frq;
+      rxnFlux[i] = m_CIISCProb * coll_frq;
     }
 
     // The flux bottom energy is equal to the ZPE of the transition state.
